d13763ae43a5009491d23d3a8e7c6859f87890f5
n7n788 <yuki1113.2001@gmail.com>
2025-06-10 00:08:38 +0900

    add: VPCの追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,9 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+// ec2に関するパッケージをインポート
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // vpcを宣言
+    const vpc = new ec2.Vpc(this, 'BlogVpc', {
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    });
   }
 }
+
