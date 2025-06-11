9ce717a7b8344863de4dcdc2be32dc60934c39bc
n7n788 <yuki1113.2001@gmail.com>
2025-06-11 23:03:44 +0900

    add: ALB、EC22台、RDSをマルチAZ構成の冗長構成に変更

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,10 +1,11 @@
-import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { readFile, readFileSync } from "fs";
 import * as rds from "aws-cdk-lib/aws-rds";
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
+
+import { WebServerInstance } from './constructs/web-server-instance';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,23 +15,13 @@ export class CdkWorkshopStack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
     });
 
-    const webServer1 = new ec2.Instance(this, 'WordPressServer1', {
+    const webServer1 = new WebServerInstance(this, 'WordPressServer1', {
       vpc,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
     });
 
-    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
-    webServer1.addUserData(script);
-
-    webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
-
-    new CfnOutput(this, "WordPressServer1PublicIPAddress", {
-      value: `http://${webServer1.instancePublicIp}`,
-    })
+    const webServer2 = new WebServerInstance(this, 'WordPressServer2', {
+      vpc,
+    });
 
     const dbInstance = new rds.DatabaseInstance(this, 'WordPressDB', {
       vpc,
@@ -39,9 +30,11 @@ export class CdkWorkshopStack extends Stack {
       }),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
       databaseName: 'wordpress',
+      multiAz: true,
     });
 
-    dbInstance.connections.allowDefaultPortFrom(webServer1);
+    dbInstance.connections.allowDefaultPortFrom(webServer1.instance);
+    dbInstance.connections.allowDefaultPortFrom(webServer2.instance);
 
     const lb = new elbv2.ApplicationLoadBalancer(this, 'LoadBalancer', {
       vpc,
@@ -55,11 +48,13 @@ export class CdkWorkshopStack extends Stack {
 
     listener.addTargets('WordPressTarget', {
       port: 8080,
-      targets: [new targets.InstanceTarget(webServer1, 80)],
+      targets: [new targets.InstanceTarget(webServer1.instance, 80),
+        new targets.InstanceTarget(webServer2.instance, 80)],
       healthCheck: { path: '/wp-includes/images/blank.gif'},
     });
 
-    webServer1.connections.allowFrom(lb, ec2.Port.tcp(80));
+    webServer1.instance.connections.allowFrom(lb, ec2.Port.tcp(80));
+    webServer2.instance.connections.allowFrom(lb, ec2.Port.tcp(80));
   }
 }
 
